83df7f841cc305e57a8c49a2b180eff8851ede3e
Rommel <wromw22@gmail.com>
2026-09-21 13:37:11 +0200

    update mini proyecto iva y porcentaje

--- Tema1/Inicio/src/Operadores.java
+++ Tema1/Inicio/src/Operadores.java
@@ -4,10 +4,20 @@ public class Operadores {
         int cantidad = 3;
         final double IVA = 0.21;
 
-        double base = precio * cantidad;
-        double total = base + base * IVA;
-        int truncado = (int) total;
+        double base = precio * cantidad; //Multiplica el precio del producto por la cantidad que hay
+        double total = base + base * IVA; //Multiplica el precio por el IVA y se suma al precio base para dar el total
+        int precioTruncado = (int) total;
 
-        System.out.printf("Base %.2f / Total %.4f / Truncado %d%n", base, total, truncado);
+
+        System.out.printf("Base %.2f / Total %.4f / Precio Truncado %d%n", base, total, precioTruncado);
+
+        /*
+        Como calcular el porcentaje ya que sin convertir el int a double habria salido 0
+         */
+        int alumnosAprobados = 21;
+        int totalAlumnos = 30;
+        double porcentajeAlumnosAprobados = (double) alumnosAprobados / totalAlumnos * 100;
+
+        System.out.println("El porcentaje de alumnos aprobados son: " + porcentajeAlumnosAprobados + "%");
     }
 }
