6d904fe41c30a1b4af59ca2acb923b9f965e4bb4
Rommel <wromw22@gmail.com>
2026-09-22 13:07:56 +0200

    primera clase de programacion

---
+++ Tema1/Componentes/src/Entrada.java
@@ -0,0 +1,17 @@
+public class Entrada {
+    //Comentarios
+    /*
+    TODO. Sirve para marcar cosas por hacer -> View/tool windows/TODO
+     */
+
+    //Para hacer un metodo ->
+    // mod_acceso (Quien tiene acceso)
+    // Retorno (Da el metodo para usarlo o no)
+    // Nombre del metodo
+    // (argumentos) lo que necesita el metodo para funcionar
+    //Ejecucucion del metodo (Como funciona)
+
+    public static void main(String[] args){
+        System.out.println("Mi primer programa");
+    }
+}
