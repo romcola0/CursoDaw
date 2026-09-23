4c5333cdca9e6f0627f564e354a2dcb3b06ce65b
Rommel <wromw22@gmail.com>
2026-09-23 14:37:57 +0200

    first class MPO

---
+++ MPO/ProyectoMPO1/src/proyecto/Main.java
@@ -0,0 +1,12 @@
+package proyecto;
+//paquetes minusculas
+public class Main {
+    //clases mayuscula
+
+    public static void main (String[] args) {
+
+        //public -> modificador de acceso (public/private/protected)
+        //static -> este metodo pertenece a la clase Main.java
+        //void -> el metodo no devuelve ningun valor
+    }
+}
