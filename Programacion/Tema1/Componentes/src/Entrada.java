e059eef172b8895e64bf9fa7d7203837a981dd59
Rommel <wromw22@gmail.com>
2026-09-23 12:41:19 +0200

    update clase 3

--- Programacion/Tema1/Componentes/src/Entrada.java
+++ Programacion/Tema1/Componentes/src/Entrada.java
@@ -12,6 +12,46 @@ public class Entrada {
     //Ejecucucion del metodo (Como funciona)
 
     public static void main(String[] args){
+
+        //Println salta de linea como un enter, print normal no.
         System.out.println("Mi primer programa");
+        System.out.println(90);
+        System.out.println("Mi primer programa " + "en 20 minutos");
+
+        //EL printf (formateo) para no concatenar con + se usan banderas (%s)
+        //%s --> Palabra
+        //%d --> Numero sin decimales
+        //%f --> Numero con decimales
+        System.out.printf("Me llamo %s con apellidos %s %s y tengo %d años\n", "Pepito", "Martin", "Martin", 20);
+
+
+        //Las variables sirven para guardar datos y utilizarlos luego -> tipos: nombre valor
+        //segun el tipo de dato tengo guardado: palabras-clave / numeros / boolean
+        //variables con lowerCamelCase -> primera en minuscula y luego mayus
+        //Las variables tienes que declararlas antes de usarlas
+        //segun el origen de dato tengo guardado: primmitivos -> int, double, boolean (en minusc) / complejos -> String(en mayus porque es una clase)
+
+        String nombreLegal = "Pepito";
+        nombreLegal = "Pepito M";
+
+        System.out.printf("Me llamo %s con apellidos %s %s y tengo %d años", nombreLegal, "Martin", "Martin", 20);
+
+        nombreLegal = "Pepito M";
+
+        System.out.printf("Me llamo %s con apellidos %s %s y tengo %d años\n", nombreLegal, "Martin", "Martin", 20);
+
+        //las variables char tiene que ser con '' en vez de ""
+
+        char letra = 'a';
+        letra = 'b';
+
+        //Los numero los puedes guardar en byte, short, long
+        int edad = 20;
+        //Floar tiene que tener la f al final
+        double altura = 1.80;
+        float alturaFloat = 1.80f;
+        //boolean solo false o true
+        boolean acierto = false;
+
     }
 }
